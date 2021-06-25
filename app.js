12e22ff7cdb08c68abcbaeac12ad6551f845170a
César <cesar.camasf@gmail.com>
2021-06-24 22:10:32 -0500

    Actualización de datos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) )
+
 app.listen(process.env.PORT || 3000,() =>{
     console.log('Corriendo en el puerto 3000');
 });
